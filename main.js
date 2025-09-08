2cb13fa0fe1e85c4637bb9bd122e1fac7d4ce802
Ana <100047730+itsapurplehaze@users.noreply.github.com>
2025-09-07 22:09:09 -0600

    try2.77

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-document.querySelector("a-scene").addEventListener("loaded", () => {
-  console.log("Escena lista, esperando detección...");
+AFRAME.registerComponent('smooth-follow', {
+  schema: {
+    lerpFactor: {type: 'number', default: 0.2}
+  },
+  tick: function () {
+    const target = this.el.parentEl.object3D;
+    const current = this.el.object3D;
+
+    current.position.lerp(target.position, this.data.lerpFactor);
+    current.quaternion.slerp(target.quaternion, this.data.lerpFactor);
+  }
 });
