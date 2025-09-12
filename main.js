fd80a9c6addd5eb697c7d2004f850c7946233787
Ana <100047730+itsapurplehaze@users.noreply.github.com>
2025-09-12 16:42:03 -0600

    bug main

--- main.js
+++ main.js
@@ -32,19 +32,6 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   });
 
-  /*Para que no tiemble el modelo
-  AFRAME.registerComponent('smooth-follow', {
-    schema: {
-      lerpFactor: {type: 'number', default: 0.1}
-    },
-    tick: function () {
-      const target = this.el.parentEl.object3D;
-      const current = this.el.object3D;
-      current.position.lerp(target.position, this.data.lerpFactor);
-      current.quaternion.slerp(target.quaternion, this.data.lerpFactor);
-    }
-  });*/
-
   // Oyentes de eventos
   sceneEl.addEventListener("arReady", (event) => {
     console.log("MindAR está listo para la detección de targets.");
