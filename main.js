590dc4107da0d55fa3a17d7fdeb56c0f9e70829a
Ana <100047730+itsapurplehaze@users.noreply.github.com>
2025-09-15 12:03:29 -0600

    u

--- main.js
+++ main.js
@@ -9,12 +9,23 @@ document.addEventListener("DOMContentLoaded", function() {
   let arReady = false;
 
   sceneEl.addEventListener("arReady", () => {
-	  arSystem = sceneEl.systems["mindar-image-system"];
+    setTimeout(() => {
+      if (sceneEl.systems["mindar-image-system"]) {
+        arSystem = sceneEl.systems["mindar-image-system"];
+        arReady = true;
+        startButton.disabled = false;
+        startButton.textContent = "INICIAR";
+        console.log("MindAR forzado a iniciar");
+      } else {
+        console.warn("MindAR no está disponible aún");
+      }
+    }, 3000);
+	  /*arSystem = sceneEl.systems["mindar-image-system"];
     arReady = true;
     console.log("MindAR está listo");
 
     startButton.disabled = false;
-    startButton.textContent = "INICIAR";
+    startButton.textContent = "INICIAR";*/
   });
 
   sceneEl.addEventListener("arError", (event) => {
