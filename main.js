1c8f189535d323d13f246426c24ccf2b171f6ab5
Ana <100047730+itsapurplehaze@users.noreply.github.com>
2025-09-15 11:52:22 -0600

    Errorlanding4

--- main.js
+++ main.js
@@ -12,6 +12,9 @@ document.addEventListener("DOMContentLoaded", function() {
 	  arSystem = sceneEl.systems["mindar-image-system"];
     arReady = true;
     console.log("MindAR está listo");
+
+    startButton.disabled = false;
+    startButton.textContent = "INICIAR";
   });
 
   sceneEl.addEventListener("arError", (event) => {
@@ -34,9 +37,6 @@ document.addEventListener("DOMContentLoaded", function() {
     
     try {
       await arSystem.start();
-      if (document.documentElement.requestFullscreen) {
-        document.documentElement.requestFullscreen();
-      }
       landingPage.classList.add('hidden');
       arContainer.classList.remove('hidden');
     } catch (e) {
@@ -55,54 +55,3 @@ document.addEventListener("DOMContentLoaded", function() {
       console.log("Bandera perdida");
     });
 }); 
-
-
-
-
-
-
-
-
-
-
-/*TODO VA DENTRO addEventListener si decido agregarlo
-
-	const exampleTarget = document.querySelector('#example-target');
-	const examplePlane = document.querySelector('#example-plane');
-	const stopButton = document.querySelector("#example-stop-button");
-	const pauseButton = document.querySelector("#example-pause-button");
-	const pauseKeepVideoButton = document.querySelector("#example-pause-keep-video-button");
-	const unpauseButton = document.querySelector("#example-unpause-button");
-
-	stopButton.addEventListener('click', () => {
-	  arSystem.stop(); // stop AR 
-	});
-	pauseButton.addEventListener('click', () => {
-	  arSystem.pause(); // pause AR, pause video
-	});
-	pauseKeepVideoButton.addEventListener('click', () => {
-	  arSystem.pause(true); // pause AR, keep video
-	});
-	unpauseButton.addEventListener('click', () => {
-	  arSystem.unpause(); // unpause AR and video
-	});
-	// arReady event triggered when ready
-	sceneEl.addEventListener("arReady", (event) => {
-	  // console.log("MindAR is ready")
-	});
-	// arError event triggered when something went wrong. Mostly browser compatbility issue
-	sceneEl.addEventListener("arError", (event) => {
-	  // console.log("MindAR failed to start")
-	});
-	// detect target found
-	exampleTarget.addEventListener("targetFound", event => {
-	  console.log("target found");
-	});
-	// detect target lost
-	exampleTarget.addEventListener("targetLost", event => {
-	  console.log("target lost");
-	});
-	// detect click event
-	examplePlane.addEventListener("click", event => {
-	  console.log("plane click");
-	});*/
